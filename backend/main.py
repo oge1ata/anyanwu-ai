7cd35a18f688a3d8be22654642963112c1f0862d
Ogechukwu Ata <yoma@Mac-2.powerhub>
2026-03-31 10:30:05 +0100

    Changed the allowed methods

--- backend/main.py
+++ backend/main.py
@@ -40,7 +40,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
-    allow_methods=["POST", "GET"],
+    allow_methods=["POST", "GET", "OPTIONS"],
     allow_headers=["Content-Type"],
 )
 
